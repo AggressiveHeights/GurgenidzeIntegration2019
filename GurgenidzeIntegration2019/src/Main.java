2089cf58dea2b841922215f6cbfdba4d84d461c2
AggressiveHeights <balboa_20@hushmail.com>
2019-03-01 22:48:36 -0500

    3/1/19
    PSI 2 Requirements locations
    
    • Create a method with arguments and return values (GeneralMethods >
    getActivityIndex)
    • Identify a method call and argument in comments (Main > main)
    • Identify a header and parameter in comments (GeneralMethods >
    getActivityIndex)
    • Use the Random class (GeneralMethods > executeActivityChosen)
    • Use the Math class (AllTheArbitraryThings > runArbitraryTwo)
    • Create if/else constructs (AllTheArbitraryThings > runArbitraryOne)
    • Create ternary constructs (GeneralMethods > executeActivityChosen)
    • Use a switch statement (GeneralMethods > ExecuteActivityChosen)
    • Compare two String objects by using the compareTo and equals methods
    and make a comment describing how == works with objectstoBeSquared
    (AllTheArbitraryThings > runArbitraryTwo)
    • Use +, -, *, /, %, ++, --, += (AllTheArbitraryThings >
    runArbitraryTwo)
    • Use relational operators (AllTheArbitraryThings > runArbitraryTwo)
    • Use conditional operators (AllTheArbitraryThings > runArbitraryTwo)
    • Describe operator precedence as a comment (AllTheArbitraryThings >
    runArbitraryTwo)
    • Create and use while loops (AllTheArbitraryThings > runArbitraryTwo)
    • Create and use for loops (AllTheArbitraryThings > runArbitraryTwo)
    • Create and use do/while loops (AllTheArbitraryThings >
    runArbitraryTwo)
    • Use break in a loop with a comment describing what it does
    (AllTheArbitraryThings > runArbitraryTwo)
    Use continue in a loop with a comment describing what it does
    (AllTheArbitraryThings > runArbitraryTwo)

--- GurgenidzeIntegration2019/src/Main.java
+++ GurgenidzeIntegration2019/src/Main.java
@@ -1,88 +1,17 @@
-import java.util.Scanner;
-
 // The Bored_Bot
 // Irakli Gurgenidze 1/24/19
-// This program contains a variety of random information and activities that are 
+// This driver class contains a variety of random information and activities that are 
 // meant to to combat boredom
 //ctrl+shift+f to auto format code
 public class Main {
 
 	public static void main(String[] args) {
-    // Display a greeting to the user
-	System.out.println("Greetings user! I'm the Bored_Bot, creation of "
-			+ "Irakli Gurgenidze. My purpose is to entertain you, "
-			+ "to combat your Boredom! Please enter a username to continue: ");
-	
-	// Get username
-	Scanner introScanner = new Scanner(System.in);
-	String username = introScanner.nextLine();
-	
-	// The Java language supports 8 primitive data types: Bytes (8 bits),
-	// Shorts (16 bits), Ints (32 bits), Longs (64 bits), Floats (32 bits)
-	// Doubles (64 bits), Booleans (true or false), and Chars (16 bits).
-	// Of these, all but booleans and chars deal with numbers. Their difference
-	// is in the size of numbers they can contain. Booleans can be assigned a 
-	// "true" or "false" value. They are used heavily and logic. Chars are Java's
-	// native text data type. The contain one character and one character only. 
-	// Chars are also the building blocks of Strings, an object class heavily 
-	// used in java, composed of an array of Chars. 
-	
-	// Here's an example of the four main variables (boolean, int double, String) 
-	// in action: 
-	
-	//String
-	System.out.println("\nYou entered \"" + username + "\" as your username. \n");
-	
-	//int
-	System.out.println("For my first trick, I will square any integer you enter: ");
-	int toBeSquared = introScanner.nextInt();
-	System.out.println("Your result is: " + (toBeSquared * toBeSquared));
-	
-	//double
-	System.out.println("\n Now, I know what you're thinking. \"What about doubles?\""
-			+ "Enter one here: ");
-	double toBeSquaredDos = introScanner.nextDouble();
-	System.out.println("Your result is: " + (toBeSquaredDos * toBeSquaredDos));
-	
-	//boolean
-	System.out.println("\n Here's a little trick...\nEnter a boolean value and I'll "
-			+ "tell you if you are right. Right about what? Well... that doesn't "
-			+ "really matter: ");
-	boolean userBoolean = introScanner.nextBoolean();
-	if(userBoolean == true) {
-		System.out.println("Congratulations! You were wrong! ");
-	}
-	else {
-		System.out.println("You guessed correctly! No, you do not win a prize. ");
-	}
-	
-	//String class methods
-	System.out.println("\nFor my next trick, I will compeltely dissect your"
-			+ " next input. Please enter a one-token String: ");
-	String anaString = introScanner.nextLine();
-	int anaLength = anaString.length();
-	String anaLowerCase = anaString.toLowerCase();
-	String anaUpperCase = anaString.toUpperCase();
-	System.out.println("\nYou entered a total of " + anaLength + " characters. "
-			+ "Here's your entry in all lowercase: " + anaLowerCase + 
-			"\n Here's yout entry in all uppercase: " + anaUpperCase);
-	
-	//format strings using escape sequences
-	System.out.println("\nNow enter a String token so I may indent it for no reason: "
-			);
-	String indInput = introScanner.nextLine();
-	System.out.println("And here it is: \n\t" + indInput);
-	
-	//Casting is taking an object out of one data type and putting it into another
-	System.out.println("\nNow I will convert your String into a double. "
-			+ "Enter a decimal: ");
-	String casting = introScanner.nextLine();
-	double doubleVarA = Double.parseDouble(casting);
-	System.out.println("\nYou entered the value " + doubleVarA + ". To prove that I"
-			+ "actually casted, here's your value squared: " + 
-			(doubleVarA * doubleVarA));
-	
-	
-	
+		// Display a greeting to the user
+		Introduction.introduction(); // this is an example of a method call
+		
+		AllTheArbitraryThings.runArbitraryPSITwo(); //
+		
+		// Main menu goes here when applicable
   }
 }
+
---
+++ GurgenidzeIntegration2019/src/MainMenu.java
@@ -0,0 +1,6 @@
+// Irakli Gurgenidze 1/24/19
+// This class consists of code that makes up the activity selection menu 
+
+public class MainMenu {
+	
+}
